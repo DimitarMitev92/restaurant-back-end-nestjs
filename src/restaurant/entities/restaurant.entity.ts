a782825a9e494fe6edab14398b900adc98a25279
Ana Popova <ana.popova83@gmail.com>
2024-02-15 13:54:59 +0200

    feat(funk): restaorant module

--- src/restaurant/entities/restaurant.entity.ts
+++ src/restaurant/entities/restaurant.entity.ts
@@ -1,1 +1,33 @@
-export class Restaurant {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('restaurant')
+export class Restaurant {
+  @PrimaryGeneratedColumn('uuid')
+  readonly id: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  readonly name: string;
+
+  @Column({ name: 'location_id', type: 'uuid', nullable: false })
+  readonly locationId: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true })
+  deletedAt: Date;
+
+  @JoinColumn({ name: 'location_id' })
+  location: Location;
+}
--- src/restaurant/restaurant.service.ts
+++ src/restaurant/restaurant.service.ts
@@ -1,26 +1,86 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
-import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
 export class RestaurantService {
-  create(createRestaurantDto: CreateRestaurantDto) {
-    return 'This action adds a new restaurant';
+  constructor(
+    @InjectRepository(Restaurant)
+    private readonly restaurantRepository: Repository<Restaurant>,
+  ) {}
+
+  async create(createRestaurantDto: CreateRestaurantDto) {
+    try {
+      const newWarehouse =
+        this.restaurantRepository.create(createRestaurantDto);
+      return await this.restaurantRepository.save(newWarehouse);
+    } catch (error) {
+      throw new BadRequestException('Impossible create', error);
+    }
   }
 
-  findAll() {
-    return `This action returns all restaurant`;
+  async findAll(): Promise<Restaurant[]> {
+    const restaurants = await this.restaurantRepository.find({
+      where: { deletedAt: null },
+    });
+    if (restaurants.length === 0) {
+      throw new NotFoundException('DB is empty!');
+    }
+    return restaurants;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} restaurant`;
+  async findOne(id: string): Promise<Restaurant> {
+    const restaurant = await this.restaurantRepository.findOne({
+      where: { id, deletedAt: null },
+    });
+    if (!restaurant) {
+      throw new NotFoundException(`Warehouse not found.`);
+    }
+    return restaurant;
   }
 
-  update(id: number, updateRestaurantDto: UpdateRestaurantDto) {
-    return `This action updates a #${id} restaurant`;
+  async update(id: string, attrs: Partial<Restaurant>) {
+    try {
+      const restaurant = await this.restaurantRepository.findOne({
+        where: { id, deletedAt: null },
+      });
+      if (!restaurant) {
+        throw new NotFoundException(`Restaurant not found!`);
+      }
+      const updatedRestaurant = { ...restaurant, ...attrs };
+      await this.restaurantRepository.save(updatedRestaurant);
+      return updatedRestaurant;
+    } catch (error) {
+      throw new BadRequestException('Update not executed', error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} restaurant`;
+  async remove(id: string): Promise<Restaurant> {
+    const restaurant = await this.restaurantRepository.findOne({
+      where: { id, deletedAt: null },
+    });
+    if (!restaurant) {
+      throw new NotFoundException(`Restaurant not found.`);
+    }
+    restaurant.deletedAt = new Date();
+    console.log('Successfully removed record.');
+    return await this.restaurantRepository.save(restaurant);
+  }
+
+  async permanentDelete(id: string) {
+    const restaurant = await this.restaurantRepository.findOne({
+      where: { id },
+    });
+    if (!restaurant) {
+      throw new NotFoundException(`Restaurant not found.`);
+    }
+    console.log(`Warehouse permanently removed.`);
+    return await this.restaurantRepository.remove(restaurant);
   }
 }
