9b6aa108ec83dcf9b01e4d2a5fa3ec478b120c84
Radina Georgieva <radina940729@gmail.com>
2024-02-15 12:28:36 +0200

    feat:add basic crud operations for package

--- src/package/package.service.ts
+++ src/package/package.service.ts
@@ -1,26 +1,79 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePackageDto } from './dto/create-package.dto';
 import { UpdatePackageDto } from './dto/update-package.dto';
-
+import { Package } from './entities/package.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 @Injectable()
 export class PackageService {
-  create(createPackageDto: CreatePackageDto) {
-    return 'This action adds a new package';
+  constructor(
+    @InjectRepository(Package) private packageRepo: Repository<Package>,
+  ) {}
+  async create(createPackageDto: CreatePackageDto) {
+    const existingPackage = await this.packageRepo.findOne({
+      where: { type: createPackageDto.type },
+    });
+
+    if (existingPackage) {
+      throw new BadRequestException('Package with this type already exists');
+    }
+
+    const newPackage = this.packageRepo.create(createPackageDto);
+    return this.packageRepo.save(newPackage);
   }
 
-  findAll() {
-    return `This action returns all package`;
+  async findAll(type: string): Promise<Package[]> {
+    return this.packageRepo.find({ where: { type } });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} package`;
+  async findOne(id: string): Promise<Package> {
+    const pack = await this.packageRepo.findOneBy({ id });
+    if (!pack) {
+      throw new NotFoundException(`Package with ID ${id} not found`);
+    }
+    return pack;
   }
 
-  update(id: number, updatePackageDto: UpdatePackageDto) {
-    return `This action updates a #${id} package`;
+  async update(
+    id: string,
+    UpdatePackageDto: UpdatePackageDto,
+  ): Promise<Package> {
+    const pack = await this.findOne(id);
+
+    if (!pack) {
+      throw new NotFoundException(`Package with ID ${id} not found`);
+    }
+    const updatedPackage = { ...pack, ...UpdatePackageDto };
+
+    return this.packageRepo.save(updatedPackage);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} package`;
+  async removeSoft(id: string) {
+    const pack = await this.findOne(id);
+
+    if (!pack) {
+      throw new NotFoundException('Pack not found');
+    }
+
+    await this.packageRepo.softRemove(pack);
+    return {
+      success: true,
+      message: `Soft delete package successful with id ${id}`,
+    };
+  }
+
+  async removePermanent(id: string) {
+    const pack = await this.findOne(id);
+
+    if (!pack) {
+      throw new NotFoundException('Package not found');
+    }
+
+    await this.packageRepo.remove(pack);
+    return {
+      success: true,
+      message: `Permanent delete package successful with id ${id}`,
+    };
   }
 }
