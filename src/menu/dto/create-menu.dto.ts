0d5e3561911d0fd9b02b9f265f07e1f0ed0c782d
Radina Georgieva <radina940729@gmail.com>
2024-02-15 15:33:13 +0200

    feat:add create menu dto

--- src/menu/dto/create-menu.dto.ts
+++ src/menu/dto/create-menu.dto.ts
@@ -1,1 +1,11 @@
-export class CreateMenuDto {}
+import { IsUUID, IsNotEmpty, IsString, IsEnum } from 'class-validator';
+import { Type } from '../entities/menu.entity';
+export class CreateMenuDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsEnum(Type)
+  type: string;
+  @IsNotEmpty()
+  @IsUUID()
+  restaurantId: string;
+}
