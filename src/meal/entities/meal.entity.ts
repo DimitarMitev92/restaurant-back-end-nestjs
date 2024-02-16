f5e18720184f3d9aff8567bc9418f3d4d869b284
Radina Georgieva <radina940729@gmail.com>
2024-02-16 13:36:26 +0200

    feat(meal):add meal entity

--- src/meal/entities/meal.entity.ts
+++ src/meal/entities/meal.entity.ts
@@ -1,1 +1,61 @@
-export class Meal {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  Column,
+} from 'typeorm';
+@Entity()
+export class Meal {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'name' })
+  name: string;
+
+  @Column({ name: 'picture' })
+  picture: string;
+
+  @Column({ name: 'description' })
+  description: string;
+
+  @Column({ name: 'additional_note' })
+  additionalNote: string;
+
+  @Column({ name: 'start_date', type: 'date' })
+  startDate: Date;
+
+  @Column({ name: 'end_date', type: 'date' })
+  endDate: Date;
+
+  @Column({ name: 'start_hour', type: 'time' })
+  startHour: string;
+
+  @CreateDateColumn({ name: 'end_hour', type: 'time' })
+  endHour: string;
+
+  @Column('double precision')
+  price: number;
+
+  @Column({ name: 'weight' })
+  weight: number;
+
+  @Column({ name: 'menu_id' })
+  menuId: string;
+
+  @Column({ name: 'category_id' })
+  categoryId: string;
+
+  @Column({ name: 'package_id' })
+  packageId: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date;
+}
