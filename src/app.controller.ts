d7d000571028799913b90677261fa2d611f21981
Dimitar Mitev <dimitar.mitev@yara.com>
2024-02-19 13:25:14 +0200

    fix(main): remove initial app service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { Public } from './auth/public.decorator';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  @Public()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-}
+export class AppController {}
