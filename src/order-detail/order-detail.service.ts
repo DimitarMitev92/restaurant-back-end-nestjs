f3fe8c97b55aa256c4af787e79b0a3910da18714
Ana Popova <ana.popova83@gmail.com>
2024-02-25 20:56:27 +0200

    feat(func): client bill details after order created

--- src/order-detail/order-detail.service.ts
+++ src/order-detail/order-detail.service.ts
@@ -10,6 +10,7 @@ import { UpdateOrderDetailDto } from './dto/update-order-detail.dto';
 import { Meal } from 'src/meal/entities/meal.entity';
 import { Menu } from 'src/menu/entities/menu.entity';
 import { Restaurant } from 'src/restaurant/entities/restaurant.entity';
+import { Order } from 'src/order/entities/order.entity';
 @Injectable()
 export class OrderDetailService {
   constructor(
@@ -111,6 +112,32 @@ export class OrderDetailService {
       .groupBy('meal.id, meal.name, restaurant.name')
       .orderBy('order_count', 'DESC')
       .limit(1);
+
+    return query;
+  }
+
+  async getClientBill(clientId: string) {
+    const query = await this.orderDetailRepository
+      .createQueryBuilder('od')
+      .select([
+        'od.id AS order_detail_id',
+        'od.meal_id',
+        'meal.name AS meal_name',
+        'meal.description AS meal_description',
+        'od.count',
+        'meal.price AS meal_price',
+        'od.total_price AS order_total_price',
+        'o.id AS order_id',
+        'o.createdAt AS order_date_issued',
+        'o.pick_method',
+        'o.additional_info',
+      ])
+      .innerJoin(Meal, 'meal', '"meal"."id" = "od"."meal_id"')
+      .innerJoin(Menu, 'mn', '"mn"."id" = "meal"."menu_id"')
+      .innerJoin(Restaurant, 'r', '"r"."id" = "mn"."restaurant_id"')
+      .innerJoin(Order, 'o', '"od"."order_id" = "o"."id"')
+      .where('o.client_id = :clientId', { clientId })
+      .getRawMany();
 
     return query;
   }
