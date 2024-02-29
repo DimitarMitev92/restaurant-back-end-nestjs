012fa343ef20e1646002d98154c9966de890e9e0
Dimitar Mitev <dimitar.mitev@yara.com>
2024-02-29 14:38:58 +0200

    fix(main): remove duplicate enableCors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
   await app.listen(process.env.PORT);
 }
 
